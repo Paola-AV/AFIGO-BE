aa3f10de4b49b2ce696ebbcf778f07ead0cb87f9
Paola Arrieta <vargasp437@gmail.com>
2026-01-26 18:22:44 -0600

    update

--- AfigoBackend.app/AfigoBackend.Api/Program.cs
+++ AfigoBackend.app/AfigoBackend.Api/Program.cs
@@ -9,6 +9,25 @@ builder.Services.AddInfrastructure(builder.Configuration);
 // Controllers
 builder.Services.AddControllers();
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:3000",  
+            "http://localhost:5173",  
+            "https://localhost:3000",
+            "https://localhost:5173"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+      
+    });
+});
+
+
+
 // Swagger UI (Swashbuckle)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,7 +40,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();     // UI en /swagger   
 }
-
+app.UseCors("AllowFrontend");
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
--- AfigoBackend.app/AfigoBackend.Aplication/obj/Debug/net9.0/AfigoBackend.Aplication.AssemblyInfo.cs
+++ AfigoBackend.app/AfigoBackend.Aplication/obj/Debug/net9.0/AfigoBackend.Aplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AfigoBackend.Aplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e7ffd0575cf9b9cd1e0c3837e4066e5565714f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+212f1fd94df068f4c48449e2dd6ae242efa7e502")]
 [assembly: System.Reflection.AssemblyProductAttribute("AfigoBackend.Aplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AfigoBackend.Aplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
